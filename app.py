4d6d4bb39c2cfe763e317e9ed4f2ef1e126e520c
gusalseh <maroonever96@gmail.com>
2021-08-18 13:09:43 +0900

    도현민

--- app.py
+++ app.py
@@ -1322,7 +1322,7 @@ def menti_mypage_mydata(nickname):
 
     return render_template('menti_mypage_mydata.html', param=i, my_data=my_data, menti_info=menti_info, me_info=me_info,
                            action_mentor=action_mentor_array, nonaction_mentor=nonaction_mentor_array, status=status,
-                           my_alert=my_alert, token_receive=token_receive)
+                           my_alert=my_alert, token_receive=token_receive, document=document)
 
 
 @app.route('/menti_mypage_mystory/<nickname>')
