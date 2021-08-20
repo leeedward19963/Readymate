897ec2ddbcb48ad1a90a7cab7939028958dcd7b2
leeedward19963 <leeedward19963@gmail.com>
2021-08-20 11:41:01 +0900

    이호진

--- app.py
+++ app.py
@@ -6693,5 +6693,10 @@ def pay_cancel(menti_number):
             return jsonify({"result": "success"})
 
 
+@app.route('/callback', methods=['POST'])
+def callback():
+    return 'OK'
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
